5bab62c9b55834e73c8a59c6aa1e8ffa6807c9f1
KiteashSKHU <kiteash1023@office.skhu.ac.kr>
2025-09-26 16:13:32 +0900

    Main클래스, Dice클래스 주석 추가
    Dice 클래스 난수 생성 간소화 구현 필요

--- src/Dice.java
+++ src/Dice.java
@@ -1,19 +1,20 @@
 public class Dice {
 
+	//프라이빗 변수 선언
 	private int max, min;
 	private int diceResult;
 
-	public void range(int min, int max) {
+	public void range(int min, int max) {	//주사위 범위를 전달받아 max, min 변수에 저장
 		this.max = max;
 		this.min = min;
 	}
 
-	public void roll() {
+	public void roll() {	//난수를 생성하여 출력, **Math.floor 기능 활용하여 간소화 구현 필요**
 		diceResult = (int) (Math.random() * (max - min + 1)) + min;
 		System.out.println(diceResult);
 	}
 
-	public void info(int rollCount) {
+	public void info(int rollCount) {	//Main 클래스로부터 주사위를 굴릴 횟수를 전달받고, 주사위의 모든 정보를 출력
 		System.out.print(min + " ~ " + max + "의 주사위 ");
 		System.out.println(rollCount + "번 결과");
 	}
--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		Dice dice = new Dice();
-		int rollCount;
+		Dice dice = new Dice(); //주사위 dice 객체 생성
+		int rollCount;	//굴릴 횟수 저장
 
 		System.out.print("주사위의 범위 설정 (예: 1 6)>>");
-		dice.range(scan.nextInt(), scan.nextInt());
+		dice.range(scan.nextInt(), scan.nextInt());	//주사위의 범위 입력받음
 		
 		System.out.print("주사위를 굴릴 횟수 설정>>");
-		rollCount = scan.nextInt();
+		rollCount = scan.nextInt();	//주사위를 굴릴 횟수 입력받아 rollCount 변수에 저장
 		
-		dice.info(rollCount);
+		dice.info(rollCount);	//주사위 정보 출력 메소드에 굴리는 횟수 인자 전달
 		
-		for (int i = 0; i < rollCount; i++) {
+		for (int i = 0; i < rollCount; i++) { //rollCount 횟수 만큼 반복, roll 메소드 실행
 		dice.roll();
 		}
 		
